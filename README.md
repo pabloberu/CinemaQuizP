48f46cf93f0d9c7c1110b7497ebf97751de2d4c0
pabloberu <pichuchino2019@gmail.com>
2022-11-16 18:05:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+
+# 📽️ Cinema Quiz 🎞️
+
+Cinema Quiz is a movie trivia game created as the first project of the Hack A Boss coding bootcamp.
+
+
+## ❕Description
+The game is simple.
+You must correctly answer ten questions about the cinephile culture.
+At the end, you can play again or even share the game with your friends to see who is the best.
+## 🧠 Knowledge used:
+- DOM Manipulation
+- URL fetch
+- JS Async & promises
+- CSS, Flex & Grid
+- Responsive methods
+- Sharing in JS Vanilla
+- Deploy With Netlify
+
+
+## Screenshots
+Landing Page:
+![Landing Page](https://i.imgur.com/orDidK0.png)
+Tutorial:
+![Tutorial](https://i.imgur.com/qTW8sNV.png)
+In Game:
+![In Game](https://i.imgur.com/t8B0ZQn.png)
+Final Screen:
+![Final Screen](https://i.imgur.com/i1BfoQ4.png)
+
+
+## ❓ FAQ
+
+#### + Will there be more updates?
+
+-- Cinema Quiz was born as a simple project to learn and show knowledge, but we don't rule out working on new features for a 2.0 version.
+
+#### + Can I use the code to make my own quiz?
+
+-- Of course!
+Also, we would feel very flattered if our first project is an inspiration for others who are starting in the wonderful world of JavaScript
+
+
+# 💡Authors 
+
+
+
+
+## [![linkedin](https://img.shields.io/badge/TheBraisGF-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/TheBraisgf)
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/braisgf/)
+
+[![codewars](https://img.shields.io/badge/Codewars-B1361E?style=for-the-badge&logo=Codewars&logoColor=white)](https://www.codewars.com/users/Braisgf)
+
+[![instabio](https://img.shields.io/badge/SOCIAL_MEDIA-FFA500?style=for-the-badge&logo=rss&logoColor=white)](https://instabio.cc/3030409IOuIr3)
+
+
+
+
+## [![linkedin](https://img.shields.io/badge/pabloberu-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/pabloberu)
+
---
+++ js/script.js
@@ -0,0 +1,218 @@
+"use strict";
+//Variables Globales
+//Asignacion de botones HTML
+const questionText = document.querySelector("#questionText");
+const firstAnswer = document.querySelector("#first");
+const secondAnswer = document.querySelector("#second");
+const thirdAnswer = document.querySelector("#third");
+const fourthAnswer = document.querySelector("#fourth");
+const answerDiv = document.querySelector("#questions");
+const finalDiv = document.querySelector(".gamePrincipal");
+const main = document.querySelector("main");
+const count = document.querySelector("#count");
+let actualQuestion = 0;
+let score = 0;
+
+//Funciones
+const JSON = async (url) => {
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+
+    if (response) {
+      return data;
+    }
+  } catch (error) {
+    console.log("Hay error");
+    console.log(error.message);
+  }
+};
+
+const randomNumber = (range, outputCount) => {
+  let newArray = [];
+  for (let i = 0; i <= range; i++) {
+    newArray.push(i);
+  }
+  let result = [];
+
+  for (let i = 1; i <= outputCount; i++) {
+    const random = Math.floor(Math.random() * (range - i));
+    result.push(newArray[random]);
+    newArray[random] = newArray[range - i];
+  }
+  return result;
+};
+
+const renderQuestion = async (question) => {
+  if (actualQuestion === 10) {
+    renderFinal();
+    return 0;
+  }
+
+  let readQuestion = actualQuestion;
+  let a = await JSON("./js/quiz.json");
+  //Impresion de preguntas y respuestas
+  //Asignacion de textos desde el JSON
+  count.innerHTML = `Score: ${score} `;
+  questionText.innerHTML =
+    "<h2 id='questionText'>" + a[question[readQuestion]].question + "</h2>";
+  firstAnswer.innerHTML = a[question[readQuestion]].answers[0];
+  secondAnswer.innerHTML = a[question[readQuestion]].answers[1];
+  thirdAnswer.innerHTML = a[question[readQuestion]].answers[2];
+  fourthAnswer.innerHTML = a[question[readQuestion]].answers[3];
+};
+
+const checkAnswer = async (userAnswer, question) => {
+  let readQuestion = actualQuestion;
+  let b = await JSON("./js/quiz.json");
+  if (userAnswer === b[question[readQuestion]].correct) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const renderFinal = () => {
+  addReplay();
+  main.classList.toggle("mainScaleAni");
+  setTimeout(() => {
+    question.innerHTML = `<h2 class='questionText'>Final Score: ${score} /10</h2>`;
+  }, 500);
+  finalDiv.classList.toggle("gamePrincipal");
+  finalDiv.classList.toggle("finalVisual");
+  count.remove();
+  firstAnswer.remove();
+  secondAnswer.remove();
+  thirdAnswer.remove();
+  fourthAnswer.remove();
+  answerDiv.remove();
+};
+
+const addReplay = async () => {
+  const replayButton = document.createElement("button");
+  replayButton.classList.toggle("replayButton");
+  replayButton.innerHTML = "<a class='replay' href='index.html'>Replay</>";
+  document.querySelector(".gamePrincipal").append(replayButton);
+
+  //ADD SHARE BUTTON
+  const shareButton = document.createElement("button");
+  shareButton.classList.toggle("replayButton");
+  shareButton.innerHTML = `<a class='replay' href='whatsapp://send?text=Hey! I got a ${score}/10 points on CinemaQuiz! Try to beat me on https://cinemaquiz.netlify.app/!' data-action="share/whatsapp/share"  
+  target="_blank">Share with your friends</>`;
+  document.querySelector(".gamePrincipal").append(shareButton);
+  //SHARE FUNCTIONALITY
+  //Share data:
+  const shareData = {
+    title: "Cinema QUIZ",
+    text: `Hey! I got a ${score}/10 points on CinemaQuiz! Try to beat me`,
+    url: "https://cinemaquiz.netlify.app/",
+  };
+  shareButton.addEventListener("click", async () => {
+    try {
+      await navigator.share(shareData);
+    } catch (err) {}
+  });
+  //END OF SHARE
+};
+
+const game = () => {
+  let newQuestions = randomNumber(50, 10);
+  renderQuestion(newQuestions);
+  //PRIMERA RESPUESTA
+  firstAnswer.addEventListener("click", async (e) => {
+    const button = e.target.innerText;
+    const check = await checkAnswer(button, newQuestions);
+    if (check === true) {
+      firstAnswer.classList.toggle("correct");
+      score++;
+      actualQuestion++;
+      setTimeout(() => {
+        renderQuestion(newQuestions);
+      }, 300);
+    } else {
+      firstAnswer.classList.toggle("fail");
+      actualQuestion++;
+      setTimeout(() => {
+        renderQuestion(newQuestions);
+      }, 300);
+    }
+    setTimeout(() => {
+      firstAnswer.classList.remove("correct");
+      firstAnswer.classList.remove("fail");
+    }, 300);
+  });
+
+  //SEGUNDA RESPUESTA
+  secondAnswer.addEventListener("click", async (e) => {
+    const button = e.target.innerText;
+    const check = await checkAnswer(button, newQuestions);
+    if (check === true) {
+      secondAnswer.classList.toggle("correct");
+      score++;
+      actualQuestion++;
+      setTimeout(() => {
+        renderQuestion(newQuestions);
+      }, 300);
+    } else {
+      secondAnswer.classList.toggle("fail");
+      actualQuestion++;
+      setTimeout(() => {
+        renderQuestion(newQuestions);
+      }, 300);
+    }
+    setTimeout(() => {
+      secondAnswer.classList.remove("correct");
+      secondAnswer.classList.remove("fail");
+    }, 300);
+  });
+
+  //TERCERA RESPUESTA
+  thirdAnswer.addEventListener("click", async (e) => {
+    const button = e.target.innerText;
+    const check = await checkAnswer(button, newQuestions);
+    if (check === true) {
+      thirdAnswer.classList.toggle("correct");
+      score++;
+      actualQuestion++;
+      setTimeout(() => {
+        renderQuestion(newQuestions);
+      }, 300);
+    } else {
+      thirdAnswer.classList.toggle("fail");
+      actualQuestion++;
+      setTimeout(() => {
+        renderQuestion(newQuestions);
+      }, 300);
+    }
+    setTimeout(() => {
+      thirdAnswer.classList.remove("correct");
+      thirdAnswer.classList.remove("fail");
+    }, 300);
+  });
+
+  //CUARTA RESPUESTA
+  fourthAnswer.addEventListener("click", async (e) => {
+    const button = e.target.innerText;
+    const check = await checkAnswer(button, newQuestions);
+    if (check === true) {
+      fourthAnswer.classList.toggle("correct");
+      score++;
+      actualQuestion++;
+      setTimeout(() => {
+        renderQuestion(newQuestions);
+      }, 300);
+    } else {
+      fourthAnswer.classList.toggle("fail");
+      actualQuestion++;
+      setTimeout(() => {
+        renderQuestion(newQuestions);
+      }, 300);
+    }
+    setTimeout(() => {
+      fourthAnswer.classList.remove("correct");
+      fourthAnswer.classList.remove("fail");
+    }, 300);
+  });
+};
+
+game();
